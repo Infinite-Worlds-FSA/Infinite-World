46dd2b2f4b75c8f734dd1ddf66122d93087b80a6
abrahamflores <a.flrs182@gmail.com>
2023-05-11 09:59:20 -0700

    steady commit

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -85,7 +85,6 @@
 // export default Game;
 
 import Player from "../gameObjects/Player";
-import Debugger from "../gameObjects/Debugger";
 import Goomba from "../gameObjects/Goomba";
 import Coin from "../gameObjects/Coin";
 import Flag from "../gameObjects/Flag";
@@ -157,7 +156,6 @@ class Game extends Phaser.Scene {
     this.goombas = new Goomba(this).collideWith(this.platform);
     this.coins = new Coin(this).collideWith(this.player.sprite);
     this.flag = new Flag(this);
-    this.debugger = new Debugger(this);
 
     this.inputs = this.input.keyboard.createCursorKeys();
   }
@@ -166,7 +164,6 @@ class Game extends Phaser.Scene {
     this.player.update(this.inputs);
     this.goombas.update();
     this.coins.update();
-    this.debugger.debuggerEnabled && this.debugger.update();
   }
 }
 
