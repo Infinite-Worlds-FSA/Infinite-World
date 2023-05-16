b269110c4a608141e3ecfeb7d0b18d632a32ce9d
Hussein Aligabi <aligabih@gmail.com>
2023-05-16 15:18:24 -0400

    Added both home and gametheme audio to game and made it so that it automatically plays when in the game, I also removed any commented out code

--- src/scenes/TitleMenu.js
+++ src/scenes/TitleMenu.js
@@ -1,6 +1,11 @@
 class TitleMenu extends Phaser.Scene {
   constructor() {
     super("TitleMenu");
+    this.homeAudio = null;
+  }
+
+  preload() {
+    this.load.audio("home", "./assets/home.mp3");
   }
 
   toggleScoreDisplay(visible) {
@@ -12,35 +17,39 @@ class TitleMenu extends Phaser.Scene {
     }
   }
 
+  create() {
+    const spaceBackground = this.add.graphics({
+      fillStyle: { color: 0x1a113c },
+    });
+    spaceBackground.fillRect(
+      0,
+      0,
+      this.cameras.main.width,
+      this.cameras.main.height
+    );
 
-    create() {
-        const spaceBackground = this.add.graphics({ fillStyle: { color: 0x1a113c } }); // 0x663399 is a dark purple, if the other purple is too dark
-        spaceBackground.fillRect(0, 0, this.cameras.main.width, this.cameras.main.height);
+    const starsOverlay = this.add.graphics({ fillStyle: { color: 0xffffff } });
+    for (let i = 0; i < 200; i++) {
+      const x = Math.random() * this.cameras.main.width;
+      const y = Math.random() * this.cameras.main.height;
+      const radius = Math.random() * 2;
+      starsOverlay.fillCircle(x, y, radius);
+    }
 
-        const starsOverlay = this.add.graphics({ fillStyle: { color: 0xffffff } });
-        for (let i = 0; i < 200; i++){ // adjusting this number will change the number of stars
-            const x = Math.random() * this.cameras.main.width;
-            const y = Math.random() * this.cameras.main.height;
-            const radius = Math.random() * 2;
-            starsOverlay.fillCircle(x, y, radius);
-        };
+    this.toggleScoreDisplay(false);
 
-        this.toggleScoreDisplay(false);
+    const titleText = this.add.text(
+      this.cameras.main.width / 2,
+      this.cameras.main.height / 4,
+      "Welcome to\nInfinite Worlds",
+      { font: "80px staatliches", fill: "#FFD408", align: "center" }
+    );
+    titleText.setOrigin(0.5, 0.2);
 
-        const titleText = this.add.text(
-            this.cameras.main.width / 2,
-            this.cameras.main.height / 4,
-            'Welcome to\nInfinite Worlds',
-            { font: '80px staatliches', fill: '#FFD408', align: 'center' }
-        );
-        titleText.setOrigin(0.5, 0.2);
-
-        const buttonWidth = 200;
-        const buttonHeight = 50;
-        const x = this.cameras.main.width / 2 - buttonWidth / 2;
-        const y = this.cameras.main.height / 2 - buttonHeight / 2 + 100;
-
-
+    const buttonWidth = 200;
+    const buttonHeight = 50;
+    const x = this.cameras.main.width / 2 - buttonWidth / 2;
+    const y = this.cameras.main.height / 2 - buttonHeight / 2 + 100;
 
     const buttonGraphics = this.add.graphics({
       fillStyle: { color: 0xff0000 },
@@ -64,7 +73,8 @@ class TitleMenu extends Phaser.Scene {
       .setInteractive();
     startButton.on("pointerdown", () => {
       this.toggleScoreDisplay(true);
-      this.scene.start("Controls"); //changed this from 'Game' to 'Controls', can change back if needed to exclude controls screen
+      this.homeAudio.stop();
+      this.scene.start("Controls");
     });
 
     startButton.on("pointerover", () => {
@@ -74,6 +84,27 @@ class TitleMenu extends Phaser.Scene {
     startButton.on("pointerout", () => {
       this.game.canvas.style.cursor = "default";
     });
+
+    this.inputs = this.input.keyboard.createCursorKeys();
+
+    const loadAudio = () => {
+      this.homeAudio = this.sound.add("home", { loop: true });
+      this.homeAudio.play();
+    };
+
+    loadAudio();
+
+    this.input.once("pointerdown", loadAudio);
+    this.input.keyboard.once("keydown", loadAudio);
+    this.scene.get("Controls").events.once("start", () => {
+      this.homeAudio.stop();
+    });
+  }
+
+  shutdown() {
+    if (this.homeAudio) {
+      this.homeAudio.stop();
+    }
   }
 }
 
