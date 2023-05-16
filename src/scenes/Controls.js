b36b45bdc67093a29d538c5953b028c045177164
abrahamflores <a.flrs182@gmail.com>
2023-05-16 12:35:34 -0700

    added comments, styling, and created a shape for the spacebar, left and right arrows, positioned them spaced on the window

--- src/scenes/Controls.js
+++ src/scenes/Controls.js
@@ -34,13 +34,73 @@ class Controls extends Phaser.Scene {
         );
         title.setOrigin(0.5, 1.0);
 
+        //spacebar
+        const outerBox = this.add.graphics({ fillStyle: { color: 0xaaaaaa } });
+        const outerBoxWidth = 200;
+        const outerBoxHeight = 50;
+        const outerBoxX = this.cameras.main.width / 2 - outerBoxWidth / 2;
+        const outerBoxY = this.cameras.main.height * 7 / 8 - outerBoxHeight / 2; // this moves the position of the box up and down
+        outerBox.fillRect(outerBoxX, outerBoxY, outerBoxWidth, outerBoxHeight);
+
+        const innerBox = this.add.graphics({ fillStyle: { color: 0x777777 } });
+        const padding = 5;
+        const innerBoxWidth = outerBoxWidth - 2 * padding;
+        const innerBoxHeight = outerBoxHeight - 2 * padding;
+        const innerBoxX = outerBoxX + padding;
+        const innerBoxY = outerBoxY + padding;
+        innerBox.fillRect(innerBoxX, innerBoxY, innerBoxWidth, innerBoxHeight);
+
+        // Left & Right Keys
+        const keyWidth = 100;
+        const keyHeight = 100;
+        const keyPadding = 10;
+        const centerY = this.cameras.main.height * 9 / 16; // this moves the position of both arrows up and down
+        const centerXLeft = this.cameras.main.width / 3;
+        const centerXRight = this.cameras.main.width * 2 / 3;
+
+        const outerLeftKey = this.add.graphics({ fillStyle: { color: 0xaaaaaa } });
+        outerLeftKey.fillTriangle(
+            centerXLeft, centerY,
+            centerXLeft - keyWidth / 2, centerY - keyHeight / 2,
+            centerXLeft - keyWidth / 2, centerY + keyHeight / 2
+        );
+
+        const innerLeftKey = this.add.graphics({ fillStyle: { color: 0x777777 } });
+        innerLeftKey.fillTriangle(
+            centerXLeft, centerY,
+            centerXLeft - (keyWidth - keyPadding) / 2, centerY - (keyHeight - keyPadding) / 2,
+            centerXLeft - (keyWidth - keyPadding) / 2, centerY + (keyHeight - keyPadding) / 2
+        );
+
+        const outerRightKey = this.add.graphics({ fillStyle: { color: 0xaaaaaa } });
+        outerRightKey.fillTriangle(
+            centerXRight, centerY,
+            centerXRight + keyWidth / 2, centerY - keyHeight / 2,
+            centerXRight + keyWidth / 2, centerY + keyHeight / 2
+        );
+
+        const innerRightKey = this.add.graphics({ fillStyle: { color: 0x777777 } });
+        innerRightKey.fillTriangle(
+            centerXRight, centerY,
+            centerXRight + (keyWidth - keyPadding) / 2, centerY - (keyHeight - keyPadding) / 2,
+            centerXRight + (keyWidth - keyPadding) / 2, centerY + (keyHeight - keyPadding) / 2
+        );
+
         const controls = this.add.text(
             this.cameras.main.width / 2,
-            this.cameras.main.height / 4,
-            "\n\nArrow Keys to move left and right \n<=  =>\nSpacebar to jump \n[___]\n\n",
+            this.cameras.main.height / 2,
+            "\n\n\nSpacebar to jump\n\n",
             { font: "32px staatliches", fill: "#FFD408", align: "center" }
         )
         controls.setOrigin(0.5, 0.0);
+
+        const controls2 = this.add.text(
+            this.cameras.main.width / 2,
+            this.cameras.main.height / 4 + 10,
+            "\n\nArrow Keys to move left and right \n\n",
+            { font: "32px staatliches", fill: "#FFD408", align: "center" }
+        )
+        controls2.setOrigin(0.5, 0.3);
 
         const loadingText = this.add.text(
             10,
@@ -52,7 +112,7 @@ class Controls extends Phaser.Scene {
 
         this.time.delayedCall(6000, () => {
             this.scene.start("Game");
-          });
+        });
     };
 };
 
